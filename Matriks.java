d2b2184924587f28ad39702e517bd1c4546fba64
Ilham Syahid S <ilhamsyahids@gmail.com>
2019-09-17 22:15:52 +0700

    add Input Java, BacaM, TulisM

--- Matriks.java
+++ Matriks.java
@@ -1,24 +1,47 @@
+import java.util.*;
+
 final public class Matriks {
+    Scanner scanner = new Scanner(System.in); // Untuk Input
     private final int baris; // Baris
     private final int kolom; // Kolom
-    private double[][] mat; // isi array, double biar threadsafe
-    // Manggil isi matriks-nya matriks.mat[baris][kolom]
+    public double[][] Mat; // isi array, double biar threadsafe
+    // Manggil isi matriks-nya matriks.Mat[baris][kolom]
 
     // Konstruktor
     public Matriks(int baris, int kolom) {
         this.baris = baris;
         this.kolom = kolom;
-        this.mat = new double[baris][kolom];
+        this.Mat = new double[baris][kolom];
     }
 
+    // Baca Matriks
+    public void BacaMat() {
+        for (int i = 0; i < this.baris; i++) {
+            for (int j = 0; j < this.kolom; j++) {
+                this.Mat[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.print("\n");
+    }
+
+    // Menulis Matriks
+    public void TulisMat() {
+        for (int i = 0; i < this.baris; i++) {
+            for (int j = 0; j < this.kolom; j++) {
+                System.out.printf("%.2f ", this.Mat[i][j]);
+            }
+            System.out.print("\n");
+        }
+    }
+
+    // Nuker baris
     private void swap(int a, int b) {
-        // Nuker baris
-        double[] temp = mat[a];
-        mat[a] = mat[b];
-        mat[b] = temp;
+        double[] temp = Mat[a];
+        Mat[a] = Mat[b];
+        Mat[b] = temp;
     }
 
     double determinan(Matriks M) {
-
+        return 0;
     }
 }

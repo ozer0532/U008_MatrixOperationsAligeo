8f3ca3550dd289ef6700c58b8f43365e10297f93
Jon Felix <jonfelix1@gmail.com>
2019-09-25 22:19:32 +0700

    fix resource leak

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(System.in); // Untuk Input
-
-        Menu.Run();
-
+        
+        // Menu.Run();
+        Scanner input = new Scanner (new File("mat.txt"));
+        Matriks M = new Matriks(input);
+        M.TulisMat();
         scanner.close();
+        input.close();
     }
 }
